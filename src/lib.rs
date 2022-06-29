e91082d191c20e96a537709d80b0173dbf9f9737
Nemo Andrea <nemoandrea@gmail.com>
2022-06-29 17:49:58 +0200

    Clean up lib.rs
    Lib.rs still had some legacy code (to be used in another package) that is now removed. Added some basic docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,16 @@
+//! # Gerber-parser
+//! 
+//! This is a simple parser in rust for Gerber file (or string). The underlying representation
+//! is built on the `gerber-types` crate. 
+//! 
+//! Gerber files are the de-facto file format for PCB manufacturing, but are also used in other 
+//! context, such as in microfabrication. It is an old format with a lot of baggage, but the 
+//! [specification is well documented](https://www.ucamco.com/en/guest/downloads/gerber-format),
+//! there is an [online free viewer available](https://gerber-viewer.ucamco.com/) to check your designs,
+//! and the format is plaintext making it easy to work with. 
+//! 
+//! As the crate is still in the early version, expect significant changes over time, as both this
+//! crate and `gerber-types` will need to undergo changes.
+
 pub mod parser;
 pub mod gerber_doc;
-
-use std::ffi::OsStr;
-use std::path::{Path};
-use std::fs;
-
-pub fn test_func() {
-    println!("this is just a test, move along")
-}
-
-pub fn get_templates(template_dir: &Path) -> Vec<std::path::PathBuf> {
-    println!("Using templates in: {}", template_dir.display());
-    let raw_template_files = fs::read_dir(template_dir).unwrap();
-
-    let mut gerber_files:Vec<std::path::PathBuf> = Vec::new();
-
-    for direntry in raw_template_files {
-        let path = direntry.unwrap().path();
-        if let Some(extension) = path.extension() {
-            if extension == "gbr" {
-                gerber_files.push(path);
-            }
-        }        
-    }
-
-    let prettyfiles:Vec<&OsStr>= gerber_files.iter().map(|x| x.file_name().unwrap()).collect();
-    println!("Found {} gerber files, namely: {:?}", &gerber_files.len(), prettyfiles);
-
-    gerber_files
-}
-
